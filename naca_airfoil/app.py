8956035b7eed55e623e861b6faeacb94fe8ad47d
Johannes Henriksson <johe7425@gmail.com>
2016-10-17 16:12:15 +0200

    work divide function

--- naca_airfoil/app.py
+++ naca_airfoil/app.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from celery import group
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 import subprocess
 import sys
 import os
@@ -16,7 +16,7 @@ import math
 from tasks import runApp
 
 app = Flask(__name__)
-@app.route('/naca/api', methods=['GET', 'POST'])
+@app.route('/naca/api', methods=['GET'])
 def basic_api():
         
 	#req = urllib2.request(0,10,5,200,0)
@@ -37,42 +37,34 @@ def basic_api():
         #time_elapsed = (time.time() - startTime)	
         print "The task is done!"
         #getContainer()
-        
-        
-def createCallTuples(angle_start, angle_stop, n_angles, n_nodes, n_levels):
-        workLoad = n_levels*n_nodes*n_angles*(angle_stop-angle_start)
-        #numWorkers = ceil(workLoad) #TODO: create
-        num_workers = 3
-        tupleList = []
-        for i in range(num_workers):
-                tupleList.append((math.floor((angle_start/num_workers)*i,
-                                 math.floor((angle_stop/num_workers)*i,
-                                 math.floor(n_angles / num_workers-math.ceil(i)),
-                                 n_nodes,n_levels))
-        return tupleList
-        
 
 @app.route('/naca/service', methods=['GET', 'POST'])
 def web_api():
         if request.method == 'POST':
-	        angle_start = int(request.form['angles_start'])
-                angle_stop = int(request.form['angles_stop'])
-                n_angles = int(request.form['n_angles'])
-                n_nodes = int(request.form['n_nodes'])
-                n_levels = int(request.form['n_levels'])
+                try:
+	                angle_start = int(request.form['angles_start'])
+                        angle_stop = int(request.form['angles_stop'])
+                        n_angles = int(request.form['n_angles'])
+                        n_nodes = int(request.form['n_nodes'])
+                        n_levels = int(request.form['n_levels'])
 
-                viscosity = int(request.form['viscosity'])
-                speed = int(request.form['speed'])
-                num_samples = int(request.form['num_samples'])
-                time = int(request.form['time'])
-                
+                        viscosity = int(request.form['viscosity'])
+                        speed = int(request.form['speed'])
+                        num_samples = int(request.form['samples'])
+                        time = int(request.form['time'])
+                except:
+                        return "invalid input"
 
+                #tup = (0, 6, 3, 200,0);
+	        #tup2 = (6, 10, 2, 200,0);
+
+                #return str(time)
                 
-	        tup = (0, 6, 3, 200,0);
-	        tup2 = (6, 10, 2, 200,0);
-                
-	        tuplelist = [tup,tup2]
-	        
+                tupleList = createCallTuples(angle_start, angle_stop, n_angles, n_nodes, n_levels)
+	        #tuplelist = [tup,tup2]
+
+        
+	        return "it worked"
 	        job = group(runApp.s(*i) for i in tuplelist)
 	        dataTask = job.apply_async()
 	        print "Celery is working..."
@@ -82,9 +74,42 @@ def web_api():
                 #time_elapsed = (time.time() - startTime)	
                 print "The task is done!"
                 #getContainer()
-                return render_template('return_page.html')
+                #return render_template('return_page.html')
         else:
-                return render_template('req_file.html')
+                return render_template('grupp3.html')
+
+def createCallTuples(angle_start, angle_stop, n_angles, n_nodes, n_levels, num_workers):
+        #workLoad = n_levels*n_nodes*n_angles*(angle_stop-angle_start)
+        t = int(((angle_stop - angle_start) / num_workers))*num_workers
+        t2 = angle_stop - angle_start - t
+        nA = int(n_angles / num_workers)*num_workers
+        nA2 = n_angles - nA
+        print str(t) + " " + str(t2)
+        #numWorkers = ceil(workLoad) #TODO: create
+        tupleList = []
+        
+        tupleList.append((int((angle_start)),
+                          int(angle_stop/num_workers),
+                          int(n_angles/num_workers),
+                          n_nodes,
+                          n_levels))
+        _angle_start = 0
+        _angle_stop = 0
+        _nA = 0
+        for i in range(1, num_workers):
+                if i > num_workers - t2-1:
+                        _angle_stop += 1
+                if i > num_workers - t2:
+                        _angle_start = 1
+                if i > num_workers - nA2 - 1:
+                        _nA = 1
+                tupleList.append(((int(angle_stop/num_workers)*(i)) + _angle_start,
+                                 (int(angle_stop/num_workers)*(i+1)) + _angle_stop,
+                                 int(n_angles/num_workers)+_nA,
+                                 n_nodes  ,
+                                 n_levels))
+        return tupleList
+        
         
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
