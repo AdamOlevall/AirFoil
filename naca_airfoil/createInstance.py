1bf7e8ba98affab1e493041bf684b66f43d49e04
AdamOlevall <adamolevall@gmail.com>
2016-10-20 01:32:10 +0200

    Update createInstance.py

--- naca_airfoil/createInstance.py
+++ naca_airfoil/createInstance.py
@@ -1,38 +1,55 @@
-def createWorker(nc, workers, beginNr):
-    workerIPs = []
-    keypair = nc.keypairs.find(name="chliKey")
+import os, time, sys
 
-    for x in range(beginNr,(workers+beginNr)):
-        nc.images.list()
-        image = nc.images.find(name="CprojWorkerSnap")
-        flavor = nc.flavors.find(name="m1.medium")
+def createWorker(currentWorkers, numWorkers):
+    
+config = {'user':"olevall",
+					'key':"zo5tuRLjuL",
+					'tenant_name':"g2015034",
+					'authurl':"http://130.238.29.253:5000/v3"}
 
-        nc.networks.list()
-        network = nc.networks.find(label="ACC-Course-net")
+conn = swiftclient.client.Connection(auth_version=3, **config)
 
-        print "Getting userdata..."
-        userData = open("/home/ubuntu/cloudproject/udWorker.yml","r")
+workerIPs = []
+keypair = nova.keypairs.find(name="adamolevallkey")
 
-        print "Creating server..."
-        server = nc.servers.create(name = "CprojWorker{}".format(x), image = image, flavor = flavor, network = network, key_name = keypair.name, userdata=userData)
+for x in range(currentWorkers, (currentWorkers + numWorkers)):
+    
+    image = nova.images.find(name="ubuntu 14.04")
+    flavor = nova.flavors.find(name="m1.small")
 
-        status = server.status
-        while status == 'BUILD':
-            time.sleep(5)
-            # Retrieve the instance again so the status field updates
-            instance = nc.servers.get(server.id)
-            status = instance.status
-        print "status: %s" % status
+   
 
-        floating_ip = ''
-        while floating_ip == '':
-            iplist = nc.floating_ips.list()
-            for ip_obj in iplist:
-                if ((getattr(ip_obj,'instance_id')) == None):
-                    floating_ip = getattr(ip_obj, 'ip')
-                    workerIPs.append(floating_ip)
-                    break
+    if private_net != None:
+        net = nova.networks.find(label=private_net)
+        nics = [{'net-id': net.id}]
+    else:
+        sys.exit("private-net not defined.")
 
-        print "Attaching IP:"
-        print floating_ip
-        server.add_floating_ip(floating_ip)
+   
+
+    print "Getting userdata..."
+    ud = open('userdataW.yml', 'r')
+
+    print "Creating server..."
+    instance = nova.servers.create(name = "Group3_Worker" + str(x), image = image, flavor = flavor,nics=nics, userdata=ud,key_name = keypair.name)
+
+    inst_status = instance.status
+    while inst_status == 'BUILD':
+        time.sleep(5)
+        # Retrieve the instance again so the status field updates
+        instance = nova.servers.get(instance.id)
+        inst_status = instance.status
+    print "status: %s" % inst_status
+
+    floating_ip = ''
+    while floating_ip == '':
+        iplist = nova.floating_ips.list()
+        for ip_obj in iplist:
+            if ((getattr(ip_obj,'instance_id')) == None):
+                floating_ip = getattr(ip_obj, 'ip')
+                workerIPs.append(floating_ip)
+                break    
+
+    print "Attaching IP:"
+    print floating_ip
+    instance.add_floating_ip(floating_ip)
